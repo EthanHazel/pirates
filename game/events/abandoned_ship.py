a065f11c99c181911f09edad15e1c2eee4aceccc
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2024-04-29 21:26:43 -0400

    doing the best I can to add an event

--- game/events/abandoned_ship.py
+++ game/events/abandoned_ship.py
@@ -13,27 +13,18 @@ class AbandonedShipEthan (event.Event):
     '''
 
     def __init__ (self):
-        self.name = " drowed pirate attack"
+        self.name = " abandonded ship encounter"
+        self.description = "You come across an abandoned ship. You can search it for loot, but beware, there may be pirates aboard!"
+        self.loot = []
+        self.pirates = 1 if random.randint(1, 4) == 1 else 0
 
     def process (self, world):
-        '''Process the event. Populates a combat with Drowned monsters. The first Drowned may be modified into a "Pirate captain" by buffing its speed and health.'''
+        '''Process the event. Populates a combat with a Crazed Pirate.'''
         result = {}
-        result["message"] = "the drowned pirates are defeated!"
-        monsters = []
-        min = 2
-        uplim = 6
-        if random.randrange(2) == 0:
-            min = 1
-            uplim = 5
-            monsters.append(combat.Drowned("Pirate captain"))
-            monsters[0].speed = 1.2*monsters[0].speed
-            monsters[0].health = 2*monsters[0].health
-        n_appearing = random.randrange(min, uplim)
-        n = 1
-        while n <= n_appearing:
-            monsters.append(combat.Drowned("Drowned pirate "+str(n)))
-            n += 1
-        announce ("You are attacked by a crew of drowned pirates!")
-        combat.Combat(monsters).combat()
+        result["message"] = "the crazed pirate is defeated!"
+        crazed = []
+        crazed.append(combat.CrazedPirate("Crazed pirate"))
+        announce("You are attacked by a crazed pirate!")
+        combat.Combat(crazed).combat()
         result["newevents"] = [ self ]
         return result

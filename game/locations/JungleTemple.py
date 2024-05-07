439eb98ed7f4a54ae312eb466f45a986cda7181f
Ethan Hazel <134425461+EthanHazel@users.noreply.github.com>
2024-05-07 11:29:20 -0400

    further developed jungle temple and other optimizations

--- game/locations/JungleTemple.py
+++ game/locations/JungleTemple.py
@@ -22,7 +22,7 @@ class EthanJungleTemple(location.Location):
         self.locations["temple"] = JungleTemple(self)
 
     def enter(self, ship):
-        print("You have arrived at a dense, mysterious jungle island.")
+        announce ("You have arrived at a dense, mysterious jungle island.")
 
     def visit(self):
         config.the_player.location = self.starting_location
@@ -30,6 +30,7 @@ class EthanJungleTemple(location.Location):
         super().visit()
 
 
+# TODO: Convert this to eastEdge westEdge etc so you can find your way back to the temple.
 class JungleEdge(location.SubLocation):
     def __init__(self, m):
         super().__init__(m)
@@ -37,7 +38,7 @@ class JungleEdge(location.SubLocation):
         self.verbs['test'] = self
 
     def enter(self):
-        print("You're surrounded by tall vines. There's nothing of interest here.")
+        announce ("You're surrounded by tall vines. There's nothing of interest here.")
 
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == "test"):
@@ -49,16 +50,25 @@ class JungleEntrance(location.SubLocation):
     def __init__(self, m):
         super().__init__(m)
         self.name = "jungleEntrance"
-        self.verbs['test'] = self
+        self.verbs['enter'] = self
+        self.verbs['north'] = self
+        self.verbs['south'] = self
+        self.verbs['east'] = self
+        self.verbs['west'] = self
 
     def enter(self):
-        print("You stand at the edge of a dense jungle. You can see a mysterious temple deeper inside.")
+        announce ("You stand at the edge of a dense jungle. You can see a mysterious temple deeper inside.\nWill they enter the temple?")
     
     def process_verb (self, verb, cmd_list, nouns):
-        if (verb == "test"):
-            announce ("You return to your ship. WORK IN PROGRESS.")
+        if (verb == "south"):
+            announce ("You return to your ship.")
             config.the_player.next_loc = config.the_player.ship
             config.the_player.visiting = False
+        elif (verb == "north" or verb == "east" or verb == "west"):
+            config.the_player.next_loc = self.main_location.locations[verb]
+        if (verb == "enter"):
+            config.the_player.go = True
+            config.the_player.next_loc = self.main_location.locations["temple"]
 
 
 class JungleTemple(location.SubLocation):
@@ -68,7 +78,7 @@ class JungleTemple(location.SubLocation):
         self.verbs['enter'] = self
 
     def enter(self):
-        print("You step into the ancient temple, feeling a sense of reverence and mystery.")
+        announce ("You step into the ancient temple, feeling a sense of reverence and mystery.")
         # Add riddles to solve here, if you get them right, you go to the next room, if you get them wrong, you get attacked by a monster, if you defeat the monster, you go to the next room anyways. At the end, you get a special item, a golden claymore sword
     
     def process_verb (self, verb, cmd_list, nouns):
